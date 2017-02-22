187c04a1eb50dd9e3a935d60e323bff6cb261cb3
fsackur <freddie.sackur@hotmail.com>
2017-02-22 23:38:40 +0000

    Initial commit

---
+++ ADConnectivity/TestDnsServer.cs
@@ -0,0 +1,36 @@
+﻿using Heijden.DNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+
+namespace ADConnectivity
+{
+    [Cmdlet(VerbsDiagnostic.Test, "DnsServer")]
+    public class TestDnsServer : PSCmdlet
+    {
+        public Resolver resolver { get; private set; }
+
+
+        [Parameter(Mandatory = true, Position = 0)]
+        public IPAddress IpAddress { get; set; }
+
+        [Parameter(Mandatory = true, Position = 1)]
+        public string Domain { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            resolver = new Resolver(IpAddress, 53);
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(IpAddress);
+            WriteObject(resolver.GetHostByName(Domain));
+        }
+
+
+    }
+}
