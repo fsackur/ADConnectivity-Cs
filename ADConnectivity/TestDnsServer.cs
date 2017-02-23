58b097b39a1c4d468f80d22692848382c5e992f4
Freddie Sackur <freddie.sackur@rackspace.co.uk>
2017-02-23 17:14:59 +0000

    Added Get-DnsServer cmdlet that returns a nice array of Resolver

--- ADConnectivity/TestDnsServer.cs
+++ ADConnectivity/TestDnsServer.cs
@@ -1,4 +1,6 @@
-﻿using Heijden.DNS;
+﻿//https://www.codeproject.com/Articles/23673/DNS-NET-Resolver-C
+//Install-Package Heijden.DNS
+using Heijden.DNS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,5 +34,52 @@ namespace ADConnectivity
         }
 
 
+    }
+
+    [Cmdlet(VerbsCommon.Get, "DnsServer", DefaultParameterSetName = "IpAddress")]
+    public class GetDnsServer : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "IpAddress")]
+        public IPAddress[] IpAddress { get; set; }
+
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "Hostname")]
+        public string[] Hostname { get; set; }
+
+
+
+        protected override void ProcessRecord()
+        {
+            if (ParameterSetName == "IpAddress")
+            {
+                foreach (IPAddress ip in IpAddress)
+                {
+                    WriteObject(new Resolver(new IPEndPoint(ip, 53)));
+                    Console.WriteLine("Processed one");
+                }
+            }
+            else
+            {
+                List<IPAddress> ipList = new List<IPAddress>();
+
+                foreach (string name in Hostname)
+                {
+                    try
+                    {
+                        IPHostEntry host = System.Net.Dns.GetHostEntry(name);
+                        foreach (IPAddress ip in host.AddressList)
+                        {
+                            WriteObject(new Resolver(new IPEndPoint(ip, 53)));
+                            Console.WriteLine("Processed one");
+                        }
+                    }
+                    catch
+                    {
+                        WriteWarning(string.Format("Unable to resolve hostname {0}", name));
+                    }
+                   
+                } //end foreach name in Hostname
+            }
+
+        }  //end ProcessRecord
     }
 }
