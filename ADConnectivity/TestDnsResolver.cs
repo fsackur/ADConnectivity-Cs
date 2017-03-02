d8539665413024b0321b8156f9cff4b912126b7c
fsackur <freddie.sackur@hotmail.com>
2017-03-02 22:37:06 +0000

    Started fleshing out Test-DnsResolver

--- ADConnectivity/TestDnsResolver.cs
+++ ADConnectivity/TestDnsResolver.cs
@@ -10,22 +10,72 @@ using System.Text;
 
 namespace ADConnectivity
 {
-    [Cmdlet(VerbsDiagnostic.Test, "DnsResolver")]
+    [Cmdlet(VerbsDiagnostic.Test, "DnsResolver", DefaultParameterSetName = "A")]
+    [OutputType("bool", ParameterSetName = new string[] { "ActiveDirectory" })]
     public class TestDnsResolver : PSCmdlet
     {
-        public Resolver resolver { get; private set; }
-
-
-        [Parameter(Mandatory = true, Position = 0)]
+        [Parameter(ValueFromPipeline = true, Mandatory = true, Position = 0)]
         public Resolver DnsResolver { get; set; }
 
         [Parameter(Mandatory = true, Position = 1)]
         public string Domain { get; set; }
 
+        [Parameter(ParameterSetName = "A")]
+        public SwitchParameter A { get; set; }
+
+        [Parameter(ParameterSetName = "SRV")]
+        public SwitchParameter SRV { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = "QueryType")]
+        public QType QueryType { get; set; }
+
+        [Parameter(ParameterSetName = "ActiveDirectory")]
+        public SwitchParameter TestActiveDirectory { get; set; }
+
+
         protected override void ProcessRecord()
         {
-            WriteObject(DnsResolver);
-            WriteObject(resolver.GetHostByName(Domain));
+            if (ParameterSetName != "A" && ParameterSetName != "SRV")
+            {
+                QueryType = (QType)Enum.Parse(typeof(QType), ParameterSetName);
+            }
+
+            if (ParameterSetName != "ActiveDirectory")
+            {
+                var response = DnsResolver.Query(Domain, QueryType);
+                switch (QueryType)
+                {
+                    case QType.A:
+                        WriteObject(response.RecordsA);
+                        break;
+                    case QType.SRV:
+                        WriteObject(response.RecordsSRV);
+                        break;
+                    case QType.CNAME:
+                        WriteObject(response.RecordsCNAME);
+                        break;
+                    case QType.PTR:
+                        WriteObject(response.RecordsPTR);
+                        break;
+                    case QType.NS:
+                        WriteObject(response.RecordsNS);
+                        break;
+                    case QType.SOA:
+                        WriteObject(response.RecordsSOA);
+                        break;
+                    case QType.TXT:
+                        WriteObject(response.RecordsTXT);
+                        break;
+                    default:
+                        //You can query the Resolver object's cache
+                        break;
+                }
+
+                return;
+            } // end if != ActiveDirectory
+
+
+
         }
 
 
